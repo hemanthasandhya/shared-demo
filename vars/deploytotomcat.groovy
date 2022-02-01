18262476eb33417b84bc0865be4636e9f088a2e1
hemanthasandhya <94973482+hemanthasandhya@users.noreply.github.com>
2022-02-01 12:00:28 +0530

    Update deploytotomcat.groovy

--- vars/deploytotomcat.groovy
+++ vars/deploytotomcat.groovy
@@ -1,3 +1,9 @@
-def call(ip){
+def call(ip,credid){
+   sshagent([credid]) {
+    // copy tomcat to webhook location
+     sh "scp -o StrictHostKeyChecking=no target/javademoproject.war ubuntu@${ip}:/opt/tomcat/webapps/javademoproject.war"
+     sh "ssh ubuntu@${ip}  /opt/tomcat/bin/shutdown.sh"
+     sh "ssh ubuntu@${ip}  /opt/tomcat/bin/startup.sh"
+    }
   echo "deploy to tomcat on ip ${ip}"
 }
